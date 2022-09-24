dd24680e25410d8bdbdcaa1f787b759c0df698c9
kumarrrrrrrrrrr <104314382+kumarrrrrrrrrrr@users.noreply.github.com>
2022-09-24 10:45:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let countdown=10;
+setTimeout(() => {
+    document.querySelector(".count").innerText= countdown;
+    setTimeout(() => {
+        document.querySelector(".count").innerText= --countdown;
+        setTimeout(() => {
+            document.querySelector(".count").innerText= --countdown;
+            setTimeout(() => {
+                document.querySelector(".count").innerText= --countdown;
+                setTimeout(() => {
+                    document.querySelector(".count").innerText= --countdown;
+                    setTimeout(() => {
+                        document.querySelector(".count").innerText= --countdown;
+                        setTimeout(() => {
+                            document.querySelector(".count").innerText= --countdown;
+                            setTimeout(() => {
+                                document.querySelector(".count").innerText= --countdown;
+                                setTimeout(() => {
+                                    document.querySelector(".count").innerText= --countdown;
+                                    setTimeout(() => {
+                                        document.querySelector(".count").innerText= --countdown;
+                                        setTimeout(() => {
+                                            document.querySelector(".count").innerText= "Happy Independence Day";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
